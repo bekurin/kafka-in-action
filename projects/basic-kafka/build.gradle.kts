46b166d99fb1599b27d308878fd67ab14414b23f
bekurin <bekurin17@gmail.com>
2024-03-30 23:31:44 +0900

    feat: init producer project

---
+++ projects/basic-kafka/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "kafka"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.apache.kafka:kafka-clients:3.7.0")
+    implementation("org.slf4j:slf4j-simple:1.7.30")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
